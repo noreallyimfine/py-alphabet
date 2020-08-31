fe8f53804c9e5e71ed13f5407edc909014740be7
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-31 09:14:21 -0400

    templates of all letters

--- uppercase.py
+++ uppercase.py
@@ -1,4 +1,83 @@
 import turtle
 
 class Upper:
-    pass
+    def A():
+        pass
+    
+    def B():
+        pass
+    
+    def C():
+        pass
+    
+    def D():
+        pass
+    
+    def E():
+        pass
+    
+    def F():
+        pass
+    
+    def G():
+        pass
+    
+    def H():
+        pass
+    
+    def I():
+        pass
+    
+    def J():
+        pass
+    
+    def K():
+        pass
+    
+    def L():
+        pass
+    
+    def M():
+        pass
+    
+    def N():
+        pass
+    
+    def O():
+        pass
+    
+    def P():
+        pass
+    
+    def Q():
+        pass
+    
+    def R():
+        pass
+    
+    def R():
+        pass
+    
+    def S():
+        pass
+
+    def T():
+        pass
+    
+    def U():
+        pass
+    
+    def V():
+        pass
+    
+    def W():
+        pass
+    
+    def X():
+        pass
+    
+    def Y():
+        pass
+    
+    def Z():
+        pass
