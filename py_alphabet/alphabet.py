373ac17cb0a32a34532ab8346de2cb51a0f8e177
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-09-01 20:21:31 -0400

    change divisions to multiplications, first pass at setting each letter to end with next letter start

--- py_alphabet/alphabet.py
+++ py_alphabet/alphabet.py
@@ -10,8 +10,8 @@ class Alphabet(Turtle):
         self.scale = scale
         self.screen = Screen()
         self._starting_x = -((self.screen.screensize[0] / 2) - 20) 
-        self._starting_y = (self.screen.screensize[1] / 2) - 15)
-        self.setheading(self._starting_x, self._starting_y)
+        self._starting_y = ((self.screen.screensize[1] / 2) - 15)
+        self.setpos(self._starting_x, self._starting_y)
 
         # set pointer off by default
         if not pointer_on:
--- py_alphabet/uppercase.py
+++ py_alphabet/uppercase.py
@@ -16,152 +16,161 @@ class Upper(Alphabet):
 
 
     def A(self):
+        _, starting_y = self.pos()
         self.left(75)
         self.forward(self.scale)
         self.right(150)
         self.forward(self.scale)
+        max_x, _ = self.pos()
         self.left(180)
         self.forward(self.scale * .45)
         self.left(75)
         self.forward(30)
-        self._set_next_letter_start()
+        self._set_next_letter_start(max_x, starting_y)
     
     def B(self):
         self.left(90)
         self.forward(self.scale)
         self.right(90)
-        self.circle(-25, 180)
+        self.circle(-(self.scale * .25), 180)
         self.left(180)
-        self.circle(-25, 180)
-        self._set_next_letter_start()
+        self.circle(-(self.scale * .25), 180)
+        x, y = self.pos()
+        max_x = x + (self.scale * .25)
+        self._set_next_letter_start(max_x, y)
     
     def C(self):
-        self._set_next_letter_start()
         pass
     
     def D(self):
         self.left(90)
         self.forward(self.scale)
         self.right(90)
-        self.circle(-self.scale // 2, 180)
-        self._set_next_letter_start()
+        self.circle(-self.scale * .5, 180)
+        x, y = self.pos()
+        max_x = x + (self.scale * .5)
+        self._set_next_letter_start(max_x, y)
     
     def E(self):
         starting_x, starting_y = self.pos()
-        self.forward(self.scale // 2)
+        self.forward(self.scale * .5)
         self.move_without_draw(starting_x, starting_y)
         self.left(90)
         self.forward(self.scale)
         self.right(90)
-        self.forward(self.scale // 2)
-        self.move_without_draw(starting_x, starting_y + (self.scale // 2))
-        self.forward(self.scale // 3)
-        self._set_next_letter_start()
+        self.forward(self.scale * .5)
+        self.move_without_draw(starting_x, starting_y + (self.scale * .5))
+        self.forward(self.scale / 3)
+        max_x = starting_x + (self.scale * .5)
+        self._set_next_letter_start(max_x, starting_y)
     
     def F(self):
         starting_x, starting_y = self.pos()
         self.left(90)
         self.forward(self.scale)
         self.right(90)
-        self.forward(self.scale // 2)
-        self.move_without_draw(starting_x, starting_y + (self.scale // 2))
-        self.forward(self.scale // 3)
-        self._set_next_letter_start()
+        self.forward(self.scale * .5)
+        self.move_without_draw(starting_x, starting_y + (self.scale * .5))
+        self.forward(self.scale * .33)
+        max_x = starting_x + (self.scale * .5)
+        self._set_next_letter_start(max_x, starting_y)
     
     def G(self):
-        self._set_next_letter_start()
         pass
     
     def H(self):
         starting_x, starting_y = self.pos()
         self.left(90)
         self.forward(self.scale)
-        self.move_without_draw(starting_x, starting_y + self.scale // 2)
+        self.move_without_draw(starting_x, starting_y + self.scale * .5)
         self.right(90)
-        self.forward(self.scale // 2)
-        self.move_without_draw(starting_x + self.scale // 2, starting_y)
+        self.forward(self.scale * .5)
+        self.move_without_draw(starting_x + self.scale * .5, starting_y)
         self.left(90)
         self.forward(self.scale)
-        self._set_next_letter_start()
+        max_x, _ = self.pos()
+        self._set_next_letter_start(max_x, starting_y)
     
     def I(self):
-        self.forward(self.scale / 2)
-        self.back(self.scale / 4)
+        starting_x, starting_y = self.pos()
+        self.forward(self.scale * .5)
+        self.back(self.scale * .25)
         self.left(90)
         self.forward(self.scale)
         self.left(90)
-        self.forward(self.scale / 4)
+        self.forward(self.scale * .25)
         self.left(180)
-        self.forward(self.scale / 2)
-        self._set_next_letter_start()
+        self.forward(self.scale * .5)
+        max_x = starting_x + (self.scale * .5)
+        self._set_next_letter_start(max_x, starting_y)
     
     def J(self):
-        self._set_next_letter_start()
         pass
     
     def K(self):
+        starting_x, starting_y = self.pos()
         self.left(90)
         self.forward(self.scale)
-        self.back(self.scale / 2)
+        self.back(self.scale * .5)
         self.right(45)
-        self.forward(self.scale * .70)
-        self.back(self.scale * .70)
+        self.forward(self.scale * .7)
+        self.back(self.scale * .7)
         self.right(90)
-        self.forward(self.scale * .70)
-        self._set_next_letter_start()
+        self.forward(self.scale * .7)
+        max_x = starting_x + (self.scale * .7)
+        self._set_next_letter_start(max_x, starting_y)
     
     def L(self):
-        self.forward(self.scale / 1.5)
-        self.back(self.scale / 1.5)
+        starting_x, starting_y = self.pos()
         self.left(90)
         self.forward(self.scale)
-        self._set_next_letter_start()
-        pass
+        self.move_without_draw(starting_x, starting_y)
+        self.right(90)
+        self.forward(self.scale * .66)
+        max_x = self.xcor() 
+        self._set_next_letter_start(max_x, starting_y)
     
     def M(self):
-        self._set_next_letter_start()
         pass
     
     def N(self):
-        self._set_next_letter_start()
         pass
     
     def O(self):
-        self._set_next_letter_start()
         pass
     
     def P(self):
-        self._set_next_letter_start()
         pass
     
     def Q(self):
-        self._set_next_letter_start()
         pass
     
     def R(self):
+        starting_x, starting_y = self.pos()
         self.left(90)
         self.forward(self.scale)
         self.right(90)
-        self.circle(-self.scale // 4, 180)
+        self.circle(-self.scale * .25, 180)
         self.left(135)
         self.forward(self.scale * 0.7)
-        self._set_next_letter_start()
+        max_x = starting_x + (self.scale * .35)
+        self._set_next_letter_start(max_x, starting_y)
     
     def S(self):
-        self._set_next_letter_start()
         pass
 
     def T(self):
-        self.move_without_draw(self.xcor() + self.scale / 2, self.ycor())
+        starting_x, starting_y = self.pos()
+        self.move_without_draw(starting_x + (self.scale * .5), starting_y)
         self.left(90)
         self.forward(self.scale)
         self.left(90)
-        self.forward(self.scale / 2)
+        self.forward(self.scale * .5)
         self.back(self.scale)
-        self._set_next_letter_start()
+        max_x = starting_x + self.scale
+        self._set_next_letter_start(max_x, starting_y)
     
     def U(self):
-        self._set_next_letter_start()
         pass
     
     def V(self):
@@ -171,25 +180,24 @@ class Upper(Alphabet):
         self.forward(self.scale)
         self.left(120)
         self.forward(self.scale)
-        self._set_next_letter_start()
+        max_x = starting_x + (self.scale * .6)
+        self._set_next_letter_start(max_x, starting_y)
     
     def W(self):
-        self._set_next_letter_start()
         pass
     
     def X(self):
-        starting_x, _ = self.pos()
+        starting_x, starting_y = self.pos()
         self.left(60)
         self.forward(self.scale)
         self.move_without_draw(starting_x, self.ycor())
         self.right(120)
         self.forward(self.scale)
-        self._set_next_letter_start()
+        max_x = starting_x + (self.scale * .6)
+        self._set_next_letter_start(max_x, starting_y)
     
     def Y(self):
-        self._set_next_letter_start()
         pass
     
     def Z(self):
-        self._set_next_letter_start()
         pass
