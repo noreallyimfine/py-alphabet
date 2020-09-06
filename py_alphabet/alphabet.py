39767c8390ea0b50cc13925223d698911018d0d0
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-09-06 11:30:59 -0400

    adjust default starting point

--- py_alphabet/alphabet.py
+++ py_alphabet/alphabet.py
@@ -9,8 +9,8 @@ class Alphabet(Turtle):
         super().__init__(*args, **kwargs)
         self.scale = scale
         self.screen = Screen()
-        self._starting_x = -((self.screen.screensize()[0] * .8)) 
-        self._starting_y = ((self.screen.screensize()[1] * .8))
+        self._starting_x = -((self.screen.screensize()[0] * .9)) 
+        self._starting_y = ((self.screen.screensize()[1] * .6))
         self.move_without_draw(self._starting_x, self._starting_y)
 
         # set pointer off by default
