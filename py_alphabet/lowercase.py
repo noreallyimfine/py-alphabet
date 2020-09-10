d42d4b887b91100971e9f5e8caf2a7a29f6a7250
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-09-10 12:53:13 -0400

    add template methods for each letter

--- py_alphabet/lowercase.py
+++ py_alphabet/lowercase.py
@@ -12,3 +12,81 @@ class Lower(Alphabet):
     '''
     def __init__(self, scale=100, pointer_on=False, *args, **kwargs):
         super().__init__(scale, pointer_on, *args, **kwargs)
+    
+    def a(self):
+        pass
+
+    def b(self):
+        pass
+
+    def c(self):
+        pass
+
+    def d(self):
+        pass
+
+    def e(self):
+        pass
+
+    def f(self):
+        pass
+
+    def g(self):
+        pass
+
+    def h(self):
+        pass
+
+    def i(self):
+        pass
+
+    def j(self):
+        pass
+
+    def k(self):
+        pass
+
+    def l(self):
+        pass
+
+    def m(self):
+        pass
+
+    def n(self):
+        pass
+
+    def o(self):
+        pass
+
+    def p(self):
+        pass
+
+    def q(self):
+        pass
+
+    def r(self):
+        pass
+
+    def s(self):
+        pass
+
+    def t(self):
+        pass
+
+    def u(self):
+        pass
+
+    def v(self):
+        pass
+
+    def w(self):
+        pass
+
+    def x(self):
+        pass
+
+    def y(self):
+        pass
+
+    def z(self):
+        pass
